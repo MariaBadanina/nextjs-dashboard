f5953037ecfde3b7490c8b2a5bad157de0b7147d
Maria <maria.badanina.s@gmail.com>
2024-08-19 22:03:30 -0400

    Chapter 2 done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
+import '@/app/ui/global.css'
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
