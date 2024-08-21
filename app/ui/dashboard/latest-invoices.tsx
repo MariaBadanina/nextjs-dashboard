48b3a670009d14b4cb9ba1a9f63207b2acdced11
Maria <maria.badanina.s@gmail.com>
2024-08-21 16:40:32 -0400

    Chapter 7 done

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,12 +1,12 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
+import { ArrowPathIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
+import Image from 'next/image'
+import { lusitana } from '@/app/ui/fonts'
+import { LatestInvoice } from '@/app/lib/definitions'
 export default async function LatestInvoices({
   latestInvoices,
 }: {
-  latestInvoices: LatestInvoice[];
+  latestInvoices: LatestInvoice[]
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
@@ -16,7 +16,7 @@ export default async function LatestInvoices({
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -25,7 +25,7 @@ export default async function LatestInvoices({
                   'flex flex-row items-center justify-between py-4',
                   {
                     'border-t': i !== 0,
-                  },
+                  }
                 )}
               >
                 <div className="flex items-center">
@@ -51,14 +51,14 @@ export default async function LatestInvoices({
                   {invoice.amount}
                 </p>
               </div>
-            );
+            )
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
         </div>
       </div>
     </div>
-  );
+  )
 }
